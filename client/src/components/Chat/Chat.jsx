71fd529f96f0bd5f7a1e3cce28a9ea0319ad1393
Dominique Campitelli <dominiquecamp@outlook.com>
2024-02-13 11:55:39 -0300

    Setting pages

---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function Chat() {
+  return (
+    <div>
+      <h1>Chat</h1>
+      <input type="text" placeholder="Mensagem" />
+      <button>Enviar</button>
+    </div>
+  );
+}
