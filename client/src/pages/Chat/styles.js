2b572281a99c02e37a74aecb37344a8541ec616c
Dominique Campitelli <dominiquecamp@outlook.com>
2024-02-15 23:59:32 -0300

    List and chat

---
+++ client/src/pages/Chat/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Feed = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const UsersList = styled.div`
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const OneaOne = styled.div`
+  width: 60%;
+  isplay: flex;
+  flex-direction: column;
+`;
+
+export const TextLine = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
